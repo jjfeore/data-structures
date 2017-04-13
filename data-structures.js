8f682cb6b3048d404202f21b13e26104ad7fba9f
James Feore <jjfeore@gmail.com>
2017-04-13 16:46:29 -0700

    Added length and display() to the linked list

--- data-structures.js
+++ data-structures.js
@@ -7,8 +7,16 @@ function Node(val, next) {
 
 function LinkedList() {
   this.head = null;
+  this.length = 0;
 }
 
 LinkedList.prototype.push = function(val) {
   this.head = new Node(val, this.head);
+  this.length++;
 };
+
+LinkedList.prototype.display = function(x) {
+  if (x && x.next != null) return `[${x.val}] -> ${this.display(x.next)}`;
+  else if (x) return `[${x.val}]`;
+  return `[${this.head.val}] -> ${this.display(this.head.next)}`;
+}
